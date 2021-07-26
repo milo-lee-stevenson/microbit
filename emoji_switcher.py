29c213558f74d17af47c1afac1975a74170ecc0f
someone43 <87981376+someone43@users.noreply.github.com>
2021-07-26 13:38:29 +0100

    Update emoji_switcher.py

--- emoji_switcher.py
+++ emoji_switcher.py
@@ -6,23 +6,9 @@ def on_button_pressed_a():
                 . # # # .
                 . . . . .
     """)
-    basic.clear_screen()
-    basic.show_leds("""
-        # # . # #
-                # # . # #
-                . . . . .
-                . # # # .
-                . # . # .
-    """)
-    basic.clear_screen()
-    basic.show_leds("""
-        # # . # #
-                # # . # #
-                . . . . .
-                # . . . #
-                . # # # .
-    """)
-    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
     basic.show_leds("""
         # # . . .
                 # # . # #
@@ -30,4 +16,14 @@ def on_button_pressed_a():
                 # . . . #
                 . # # # .
     """)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_leds("""
+        # # . # #
+                # # . # #
+                . . . . .
+                . # # # .
+                . # . # .
+    """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
