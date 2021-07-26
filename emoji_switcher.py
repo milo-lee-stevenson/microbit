8b3c15a085f551131b82ed77373d2b045d11c322
someone43 <87981376+someone43@users.noreply.github.com>
2021-07-26 14:58:38 +0100

    Update emoji_switcher.py

--- emoji_switcher.py
+++ emoji_switcher.py
@@ -1,29 +1,33 @@
-def on_button_pressed_a():
-    basic.show_leds("""
-        # # . # #
-                # # . # #
-                . . . . .
-                . # # # .
-                . . . . .
-    """)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+from microbit import *
 
-def on_button_pressed_ab():
-    basic.show_leds("""
-        # # . . .
-                # # . # #
-                . . . . .
-                # . . . #
-                . # # # .
-    """)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+neutral_emoji = Image("99099:"
+                      "99099:"
+                      "00000:"
+                      "09990:"
+                      "00000")
 
-def on_button_pressed_b():
-    basic.show_leds("""
-        # # . # #
-                # # . # #
-                . . . . .
-                . # # # .
-                . # . # .
-    """)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+happy_emoji = Image("99099:"
+                    "99099:"
+                    "90009:"
+                    "09990:"
+                    "00000")
+       
+sad_emoji = Image("99099:"
+                  "99099:"
+                  "00000:"
+                  "09990:"
+                  "90009")               
+
+emojis = [neutral_emoji, happy_emoji, sad_emoji]
+
+current_emoji_index = 0
+
+while True:
+    if button_a.was_pressed():
+        current_emoji = emojis.pop(current_emoji_index)
+        display.show(current_emoji)
+        emojis.insert(current_emoji_index, current_emoji)
+        if current_emoji_index == len(emojis) - 1:
+            current_emoji_index = 0
+        else:
+            current_emoji_index = current_emoji_index + 1
